a36c1feb72b167e196a2f789bbcc778bc44a1198
Nick Fournier <nick.fournier@rsginc.com>
2023-06-19 11:53:27 -0700

    basic bts pop framework setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bts_populationsim
+
+1. Install preferred Python environment management systems. I will demonstrate using Mamba, a faster conda alternative.
+
+2. Clone the population sim git repository
+```git clone https://github.com/ActivitySim/populationsim.git ./src/populationsim```
+
+3. Create the mamba environment from the environment yaml recipe:
+```mamba env create --file bts_populationsim.yml```
+
+This will create an environment and install an editable version of populationsim as well as any supporting packages. Editable means that any changes in the `./src/populationsim` folder are reflected in the environment. This makes debugging easier to add break points or print lines.
+
---
+++ setup_environments.bat
@@ -0,0 +1,38 @@
+
+@echo on
+:: Set your conda path here
+
+mkdir sources
+set WORKING_DIR=%~dp0
+set ENV_YML=%WORKING_DIR%\bts-population-env.yml
+set CONDA_DIR=%WORKING_DIR%\mambaforge
+
+
+if not exist %CONDA_DIR% (
+	:: You can manually set a local mamba dir here
+	set CONDA_DIR=C:\Users\nick.fournier\AppData\Local\mambaforge
+)
+
+if not exist %CONDA_DIR% (
+	echo Cannot find conda directory %CONDA_DIR%, edit this batch script!
+	pause
+	exit
+)
+
+call %CONDA_DIR%\Scripts\activate.bat %CONDA_DIR%
+
+:: Install populationsim, stash source code in sources for debugging
+if not exist src\populationsim\ (
+	call git clone https://github.com/ActivitySim/populationsim.git ./src/populationsim
+)
+
+if not exist %CONDA_DIR%\envs\bts_pop (
+	call mamba env create -f ENV_YML
+)
+call mamba activate bts_pop
+
+
+call python -m pip install -e ./src/populationsim
+
+pause
+exit
