9795e89d32f984183fff71d8853c40e9632e4816
Nick Fournier <nick.fournier@rsginc.com>
2023-08-17 15:15:34 -0700

    cleanup

--- batch_run.py
+++ batch_run.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     
     DataCreator = None
 
-    # settings.STATES = ['AK', 'WY', 'VT'] # Debugging
+    settings.STATES = ['AZ', 'CA', 'KY'] # Debugging
     for states_chunk in utils.batched(settings.STATES, settings.BATCH_SIZE):
         
         if len(states_chunk) > 12:
